833a74a40905d4acd98bfccbfb8dd28c2bfbf40f
Daniel Torres <dtorres@itexico.net>
2017-06-28 18:40:13 -0500

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgxHeaderComponent } from 'ngx-duality';
+import { NgxFooterComponent } from 'ngx-duality';
+import { DualOverStyleModule } from 'ngx-duality';
 
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
@@ -9,12 +12,15 @@ import { LogoComponent } from './logo/logo.component';
 @NgModule({
   declarations: [
     AppComponent,
+    NgxHeaderComponent,
+    NgxFooterComponent,
     LogoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DualOverStyleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
