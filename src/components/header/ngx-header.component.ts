e5c7f5b1bcb71c70cc894cdf8ea19976d6e8dd54
Daniel Torres <dtorres@itexico.net>
2017-06-28 18:27:19 -0500

    Fixed height on footer and header

--- src/components/header/ngx-header.component.ts
+++ src/components/header/ngx-header.component.ts
@@ -21,11 +21,11 @@ export class NgxHeaderComponent implements OnInit {
             this.el.nativeElement.style.position = 'fixed';
             this.el.nativeElement.style.width = '100%';
             this.el.nativeElement.style.zIndex = 1000;
-            this.el.nativeElement.style.maxHeight = `${this.height}px`;
+            this.el.nativeElement.style.height = `${this.height}px`;
             this.el.nativeElement.style.backgroundColor = this.color;
         }
         if (this.height) {
-            this.el.nativeElement.children[0].style.maxHeight = `${this.height}px`;
+            this.el.nativeElement.children[0].style.height = `${this.height}px`;
             this.el.nativeElement.children[0].style.backgroundColor = this.color;
         }
   }
