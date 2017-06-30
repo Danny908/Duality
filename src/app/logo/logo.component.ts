ee9e6fd299c0bb450b966f0a14180d057bdf2a9b
Daniel Torres <dtorres@itexico.net>
2017-06-30 16:36:46 -0500

    Added header title

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent implements OnInit {
+  @Input() size = 130;
+  @Input() text: boolean;
   constructor(
+    private el: ElementRef
   ) { }
 
   ngOnInit(): void {
-
   }
 }
