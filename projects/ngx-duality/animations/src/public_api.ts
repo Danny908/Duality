a78cf0b357d2ab6fc1af78bb6e917302b5bf0a5b
Danny <danny908t@gmail.com>
2018-11-14 18:37:50 -0600

    Removed unnecessary files

--- projects/ngx-duality/animations/src/public_api.ts
+++ projects/ngx-duality/animations/src/public_api.ts
@@ -1,8 +1,4 @@
 /*
  * Public API Surface of animations
  */
-
-export * from './lib/animations.service';
-export * from './lib/animations.component';
-export * from './lib/animations.module';
 export * from './lib/expand';
